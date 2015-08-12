dfe9a8f4b141a0a4d7384df55632ac742dca19c9
Kevin <kevin.stadler@ed.ac.uk>
2015-08-12 16:20:38 +0100

    Adding documentation

---
+++ README.md
@@ -0,0 +1,21 @@
+## sonic gray goo
+
+sonic gray goo is an interactive audio-visual exploration of *gray goo*, a hypothetical end-of-the-world scenario in which out-of-control self-replicating robots consume all matter on earth while building more of themselves
+
+## links and further information
+
+<img src="https://raw.githubusercontent.com/kevinstadler/sonicgraygoo/master/doc/sonicgraygoo.png" alt="sonic gray goo" title="some goo about to do some eating" align="right" />
+
+here's some of the things that inspired me to build this thing which you might find interesting too:
+
+* [All watched over by machines of loving grace](http://vimeo.com/68299139), documentary, 2011, 3 episodes of 60 minutes each, link is only to the first episode.
+
+* [Everything is broken](https://medium.com/message/everything-is-broken-81e5f33a24e1) by Quinn Norton.
+
+* [Humans need not apply](https://www.youtube.com/watch?v=7Pq-S557XQU), documentary, 2014, 15m.
+
+* the [gray goo wikipedia article](http://en.wikipedia.org/wiki/Grey_goo)
+
+## license
+
+copyright (c) 2011-2015 Kevin Stadler
--- doc/build-handout.sh
+++ doc/build-handout.sh
@@ -4,24 +4,25 @@ if [ ! -d "qr" ]; then
   mkdir "qr"
 fi
 echo '---
-classoption: a4paper,10pt
+classoption: a5paper,10pt
 header-includes:
   - \\renewcommand{\\familydefault}{\\sfdefault}
   - \\renewcommand{\\UrlFont}{\\footnotesize}
   - \usepackage{array}
   - \\newcolumntype{V}{>{\\centering\\arraybackslash} m{.4\\linewidth} }
   - \usepackage{graphicx}
+  - \usepackage{geometry}
+  - \\geometry{left=1in,right=1in}
   - \usepackage{setspace}
   - \\doublespacing
   - \\pagenumbering{gobble}
 ---
 # sonic gray goo' > "$TARGET"
 cat handout.txt >> "$TARGET"
-echo '\\vspace{3.5in}\n\n\small{feel free to take any of the reading material with you\n\n
-get a goo for your own home:} https://github.com/kevinstadler/sonicgraygoo
-\\newpage\n\n# further reading
-\\singlespacing
-\\begin{tabular}{m{4.5in}V}' >> "$TARGET"
+# \small{more links to reading material and videos/documentaries on the back of this sheet\n\n
+echo '\\newpage\n\n# recommended video/film
+\\singlespacing\\vspace{-0.7in}
+\\begin{tabular}{m{2.75in}V}' >> "$TARGET"
 i=0
 while read line; do
   if [ -n "$line" ]; then
@@ -40,7 +41,52 @@ while read line; do
         ;;
     esac
   fi
+done < further-watching.txt
+echo '\\end{tabular}\n\n# provided reading
+\\singlespacing\\vspace{-0.7in}
+\\begin{tabular}{m{2.75in}V}' >> "$TARGET"
+while read line; do
+  if [ -n "$line" ]; then
+    echo "$line"
+    case "$line" in
+      http*)
+        i=$((i+1))
+        qrencode -o "qr/$i.eps" --type=EPS "$line"
+        epspdf "qr/$i.eps"
+        echo '\n\\url{' >> "$TARGET"
+        echo "$line} & \\includegraphics[width=1in]{qr/$i.pdf}" >> "$TARGET"
+        ;;
+      *)
+        echo '\\\\\\\\' >> "$TARGET"
+        echo "$line" >> "$TARGET"
+        ;;
+    esac
+  fi
+done < provided-reading.txt
+echo '\\end{tabular}\\newpage\n\n# recommended reading
+\\singlespacing\\vspace{-0.7in}
+\\begin{tabular}{m{2.5in}V}' >> "$TARGET"
+while read line; do
+  if [ -n "$line" ]; then
+    echo "$line"
+    case "$line" in
+      http*)
+        i=$((i+1))
+        qrencode -o "qr/$i.eps" --type=EPS "$line"
+        epspdf "qr/$i.eps"
+        echo '\n\\url{' >> "$TARGET"
+        echo "$line} & \\includegraphics[width=1in]{qr/$i.pdf}" >> "$TARGET"
+        ;;
+      *)
+        echo '\\\\\\\\' >> "$TARGET"
+        echo "$line" >> "$TARGET"
+        ;;
+    esac
+  fi
 done < further-reading.txt
-echo '\\end{tabular}' >> "$TARGET"
+echo '\\end{tabular}
+\\newpage\\vspace*{\\fill}\\footnotesize
+thoughts/questions/comments? kevin.stadler@ed.ac.uk\n\n
+get a goo for your own home: https://github.com/kevinstadler/sonicgraygoo' >> "$TARGET"
 Rscript -e "rmarkdown::render('$TARGET', rmarkdown::pdf_document(fig_width=2, fig_height=2))"
 
